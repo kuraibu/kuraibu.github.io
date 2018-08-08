60cd56fbd90b76f9d5d89b6debb04227076fa5f5
Clive Tshuma <clivetshuma@gmail.com>
2018-08-08 11:32:51 +0100

    add contact php and subject field

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'FROM: Pixels & Code';
+    $to = 'info@pixelsncode.co.uk';
+    $subject = $_POST['subject'];
+
+    $body = "From: $name\n E-mail: $email\n Message: $message";
+
+    if ($_POST['submit']){
+        if (mail($to, $subject, $body, $from)) {
+            echo '<p>Your message has been sent</p>';
+        }else{
+            echo '<p>Something went wrong, please go back and try agaiin</p>';
+        }
+    }
+?>
